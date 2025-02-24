c08d4ad997b912f227d838065df0550c555c52fa
Namilsky <alive6863@gmail.com>
2025-02-24 18:51:10 +0300

    pg_up and pg_down added

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ class MapSearcher(QMainWindow):
         super().__init__()
         uic.loadUi('mainwindow.ui', self)
         self.searchButton.clicked.connect(self.run)
+        self.delta = 1
 
     def run(self):
         self.getImage()
@@ -20,8 +21,8 @@ class MapSearcher(QMainWindow):
         api_server = "https://static-maps.yandex.ru/v1"
         lon = self.lon.text()
         lat = self.lat.text()
-        delta1 = self.spn.text()
-        delta2 = self.spn.text()
+        delta1 = str(float(self.spn.text()) + float(self.delta))
+        delta2 = str(float(self.spn.text()) + float(self.delta))
         apikey = "e2a0aacc-0eb4-49b9-93c9-4caa526805a3"
 
         params = {
@@ -43,9 +44,18 @@ class MapSearcher(QMainWindow):
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key.Key_PageUp:
-            print(1)
+            val = float(self.spn.text())
+            res = val + self.delta
+            res = min(79, round(res, 2))
+            self.spn.setText(str(res))
+            self.getImage()
+
         if event.key() == Qt.Key.Key_PageDown:
-            pass
+            val = float(self.spn.text())
+            res = val - self.delta
+            res = max(0.001, round(res, 2))
+            self.spn.setText(str(res))
+            self.getImage()
 
 
 if __name__ == '__main__':
